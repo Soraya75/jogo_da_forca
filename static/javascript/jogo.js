b7ec1c428061da85d2efd8d283b6d6f5cd9f703c
Soraya75 <sorayapereiragomes935@gmail.com>
2024-09-30 14:33:05 -0300

    Update jogo.js

--- static/javascript/jogo.js
+++ static/javascript/jogo.js
@@ -3,15 +3,37 @@ let letrasAdivinhadas = [];
 const tentativasMaximas = 6;
 let tentativasRestantes;
 let palavraAtual;
+
 const palavras = [
-    "computador",
+    "telecomunicações",
+    "sistema",
+    "redes",
     "internet",
-    "programacao"
+    "protocolo",
+    "programação",
+    "banda larga",
+    "latência",
+    "protocolos",
+    "modem",
+    "roteador",
+    "fibra óptica",
+    "4G"
 ];
+
 const dicas = [
-    "Dispositivo eletrônico para processamento de dados",
-    "Rede mundial de computadores",
-    "Processo de criação de software"
+    "Comunicação à distância.",
+    "Conjunto organizado de partes.",
+    "Conexões entre pontos.",
+    "Rede mundial de computadores.",
+    "Conjunto de regras para comunicação.",
+    "Escrita de código para computadores.",
+    "Conexão de alta velocidade para transmitir dados.",
+    "Tempo de resposta entre envio e recebimento de dados.",
+    "Regras para comunicação entre dispositivos.",
+    "Dispositivo que modula e demodula sinais para comunicação.",
+    "Equipamento que direciona o tráfego de dados entre redes.",
+    "Tecnologia que usa luz para transmitir dados a alta velocidade.",
+    "Tecnologia de rede móvel."
 ];
 
 function iniciarJogo() {
